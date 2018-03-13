d4fe2e55d0e96aa21ba047313daacef778a44e88
Max Moede <max.j.moede@gmail.com>
2018-03-13 15:44:16 -0700

    uploaded lab1

--- lab2/ViewController.swift
+++ lab2/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var otherColorLabel: UILabel!
+    @IBOutlet weak var textFieldLabel: UILabel!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorLabel.text = "Status"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func segmentThing(_ sender: UISegmentedControl) {
+        myLabel.text = sender.titleForSegment(at: sender.selectedSegmentIndex)
+    }
+    
+    @IBAction func sliderThing(_ sender: UISlider) {
+        if (sender.value <= 33){
+            colorLabel.textColor = .red
+        } else if (sender.value > 33 && sender.value <= 66){
+            colorLabel.textColor = .yellow
+        } else {
+            colorLabel.textColor = .green
+        }
+        otherColorLabel.text = String(sender.value)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldLabel.text = textField.text
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
